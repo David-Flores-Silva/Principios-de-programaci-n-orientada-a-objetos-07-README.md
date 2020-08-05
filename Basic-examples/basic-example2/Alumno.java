caad5970be3b39055143869edb724c29fdb0fed9
David-Flores-Silva <68410195+David-Flores-Silva@users.noreply.github.com>
2020-08-05 17:49:17 -0500

    Add files via upload

---
+++ Basic-examples/basic-example2/Alumno.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Alumno {
+	Scanner scan=new Scanner(System.in);
+	
+	String nombre;
+	Date fechaDeNacimiento;
+	boolean genero;
+	
+	
+	
+	
+	public void estudiar() {
+		System.out.println("Estoy estudiando");
+	}
+	
+	public void dormir() {
+		System.out.println("Estoy durmiendo");
+	}
+	
+	public void comer() {
+		System.out.println("Estoy comiendo");
+	}
+	
+
+}
